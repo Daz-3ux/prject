51a935446ca22cd7189c23a4e20793c7c83614c6
daz-3ux <daz-3ux@proton.me>
2023-04-12 22:24:58 +0800

    add comment for group
    Signed-off-by: daz-3ux <daz-3ux@proton.me>

--- projectGee/gee/gee.go
+++ projectGee/gee/gee.go
@@ -8,31 +8,49 @@ import (
 
 type HandlerFunc func(*Context)
 
+// 构建支持分组和中间件的 HTTP 框架
 type (
-	RouteGroup struct {
+	// 加入分组: 以相同前缀区分
+	// 中间件支持: 中间件可以给框架提供无限扩展能力(与分组绑定,支持中间件链式调用)
+	RouteGroup struct { // 代表一个路由分组
+		// 该分组的路由前缀
 		prefix      string
+		// 该分组的中间件列表,支持中间件链式调用
 		middlewares []HandlerFunc // support middleware
+		// 该分组的父级分组
 		parent      *RouteGroup   // support nesting
+		// 整个框架的资源都是由 Engine 统一协调的,可以通过 Engine 间接的访问各类接口
 		engine      *Engine       // all groups share a Engine instance
 	}
 
-	// 整个框架的资源都是由 Engine 统一协调的
+	// 最顶层分组: 整个框架的资源都是由 Engine 统一协调的
 	Engine struct {
-		*RouteGroup
-		router *router
-		groups []*RouteGroup // store all groups
+		// 用于管理顶层的路由分组
+		*RouteGroup						// 继承了 RouteGroup 的所有属性和方法(结构体的继承)
+		// 处理 HTTP 请求
+		router *router				// 路由器实例
+		// 用于存储管理所有的路由分组
+		Groups []*RouteGroup 	// 所有分组的列表
 	}
 )
 
+/*
+	r := gee.New()
+	v1 := r.Group("/v1")
+*/
+
 func New() *Engine {
-	//return &Engine{router: newRouter()}
+	// 初始化一个新的 Engine 实例
 	engine := &Engine{router: newRouter()}
+	// 创建一个新的路由实例
 	engine.RouteGroup = &RouteGroup{engine: engine}
-	engine.groups = []*RouteGroup{engine.RouteGroup}
+	// 创建一个顶层路由分组
+	engine.Groups = []*RouteGroup{engine.RouteGroup} // Groups中的第一个元素在此处添加,为 nil
 
 	return engine
 }
 
+// 创建一个新的路由分组,并将其添加到路由器实例的分组列表中
 func (group *RouteGroup) Group(prefix string) *RouteGroup {
 	engine := group.engine
 	newGroup := &RouteGroup{
@@ -40,7 +58,8 @@ func (group *RouteGroup) Group(prefix string) *RouteGroup {
 		parent: group,
 		engine: engine,
 	}
-	engine.groups = append(engine.groups, newGroup)
+	//fmt.Printf("NewGroup: prefix=%s, parent=%v, engine=%v\n", newGroup.prefix, newGroup.parent, newGroup.engine)
+	engine.Groups = append(engine.Groups, newGroup)
 	return newGroup
 }
 
--- projectGee/main.go
+++ projectGee/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	//"fmt"
 	"net/http"
 
 	"gee"
@@ -14,7 +15,7 @@ func main() {
 	})
 
 	v1 := r.Group("/v1") 
-	{
+	{ // just for read
 		v1.GET("/hello", func(c *gee.Context) {
 			// expect /hello?name=daz
 			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
@@ -25,7 +26,7 @@ func main() {
 	}
 
 	v2 := r.Group("/v2")
-	{
+	{	// just for read
 		v2.GET("/hello/:name", func(c *gee.Context) {
 			// expect /hello/daz
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
@@ -40,6 +41,8 @@ func main() {
 			})
 		})
 	}
+
+	//fmt.Printf("%p -> %v, %p -> %v, %p -> %v\n", r.Groups[0], *r.Groups[0], r.Groups[1], *r.Groups[1], r.Groups[2], *r.Groups[2])
 
 	// RUN start webserver
 	r.RUN(":9999")
